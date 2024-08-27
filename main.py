50cbf330b232f7d46928d4738384c1ebbc7c7de9
alexnakov <alex.nakov3@gmail.com>
2024-08-27 12:58:25 +0100

    About to change get_times()

--- main.py
+++ main.py
@@ -18,28 +18,20 @@ import os
 import traceback
 import pprint
 
-# .env
+# .env and constants
 load_dotenv()
 BIN_ID = os.getenv('BIN_ID')
 API_KEY = os.getenv('API_KEY')
 DATA_POINTS = int(os.getenv('DATA_POINTS')) # Number of journeys I want to scrape
+DISPLAY_DATE_FORMAT = r'%Y-%m-%d'
+TRIP_COM_DATE_FORMAT = r'%d/%m/%Y'
 
 # query string: Tomorrow 8pm.
 now = datetime.now()
 selected_date = now.replace(hour=20, minute=0, second=0, microsecond=0) + timedelta(days=1)
-date_str = selected_date.strftime(f'%Y-%m-%d')
-trip_com_q_string = f'https://uk.trip.com/trains/list?departurecitycode=GB2278&arrivalcitycode=GB1594&departurecity=Sheffield&arrivalcity=London%20(Any)&departdate={date_str}&departhouript=22&departminuteipt=00&scheduleType=single&hidadultnum=1&hidchildnum=0&railcards=%7B%22YNG%22%3A1%7D&isregularlink=1&biztype=UK&locale=en-GB&curr=GBP'
-
-def find_elements(selector, query):
-    """ Tries to find an element within 15 secs and returns it. """
-    attempts = 0
-    while attempts < 10:
-        try:
-            return WebDriverWait(driver, 15).until(EC.presence_of_all_elements_located((selector, query)))
-        except (NoSuchElementException, TimeoutException, StaleElementReferenceException):
-            attempts += 1
-            time.sleep(1)
-    return "Could not find element you were looking for"
+date_str = selected_date.strftime(TRIP_COM_DATE_FORMAT)
+date_hour = selected_date.hour
+trip_com_q_string = f'https://uk.trip.com/trains/list?departurecitycode=GB2278&arrivalcitycode=GB1594&departurecity=Sheffield&arrivalcity=London%20(Any)&departdate={date_str}&departhouript={date_hour}&departminuteipt=00&scheduleType=single&hidadultnum=1&hidchildnum=0&railcards=%7B%22YNG%22%3A1%7D&isregularlink=1&biztype=UK&locale=en-GB&curr=GBP'
            
 def decline_cookies():
     attempts = 0
@@ -62,21 +54,16 @@ def decline_cookies():
                 print('stale exception for decline_cookies() is now clear')
             attempts = 0
 
-def get_last_time_element():
-    take_screenshot()
+def find_elements(selector, query):
+    """ Tries to find an element within 15 secs and returns it. """
     attempts = 0
     while attempts < 10:
         try:
-            all_h4s = find_elements(By.TAG_NAME, 'h4')
-            return_list = list(filter(lambda el: ':' in el.text, all_h4s))
-            return return_list[-2]
-        except StaleElementReferenceException:
+            return WebDriverWait(driver, 15).until(EC.presence_of_all_elements_located((selector, query)))
+        except (NoSuchElementException, TimeoutException, StaleElementReferenceException):
             attempts += 1
             time.sleep(1)
-            print(f'stale exception #{attempts} in get_last_time_element() occured')
-        finally:
-            if attempts > 0:
-                print('stale exception for get_last_time_element() is now clear')
+    return "Could not find element you were looking for"
 
 def get_times():
     take_screenshot()
@@ -85,6 +72,16 @@ def get_times():
         try:
             all_h4s = find_elements(By.TAG_NAME, 'h4')
             return_list = list(map(lambda x:x.text, list(filter(lambda el: ':' in el.text, all_h4s))))
+            
+            times0 = return_list[::2]
+            hour1 = 0
+            for time_txt in times0:
+                hour2 = int(time_txt[:2])
+                if hour2 < hour1:
+                    selected_date += timedelta(days=1)
+
+                hour1 = hour2
+
             return return_list
         except StaleElementReferenceException:
             attempts += 1
@@ -109,28 +106,6 @@ def get_prices():
         finally:
             if attempts > 0:
                 print('stale exception for get_prices() is now clear')
-
-def click_next_btn():
-    take_screenshot()
-    attempts = 0
-    while attempts < 10:
-        try:
-            all_divs = find_elements(By.TAG_NAME, 'div')
-            btn_as_list = list(filter(lambda el: 'View later trains' in el.text and len(el.text) == 17, all_divs))
-            if len(btn_as_list) == 0:
-                pass
-            else:
-                btn_as_list[0].click()
-                attempts = 0
-                break
-        except StaleElementReferenceException:
-            attempts += 1
-            time.sleep(1)
-            print(f'stale exception #{attempts} in click_next_btn() occured')
-        finally:
-            if attempts > 0:
-                print('stale exception for click_next_btn() is now clear')
-                break
 
 def write_to_txt_file(time0,time1,price):
     with open('real_data.txt','a') as file:
@@ -159,11 +134,49 @@ def setup_data_file():
     except:
         pass 
 
+def get_last_time_element():
+    take_screenshot()
+    attempts = 0
+    while attempts < 10:
+        try:
+            all_h4s = find_elements(By.TAG_NAME, 'h4')
+            return_list = list(filter(lambda el: ':' in el.text, all_h4s))
+            return return_list[-2]
+        except StaleElementReferenceException:
+            attempts += 1
+            time.sleep(1)
+            print(f'stale exception #{attempts} in get_last_time_element() occured')
+        finally:
+            if attempts > 0:
+                print('stale exception for get_last_time_element() is now clear')
+
 def scroll_to_next_btn(action_chains):
     last_time_element = get_last_time_element()
     take_screenshot()
     action_chains.move_to_element(last_time_element).scroll_by_amount(0, 200).perform()
     take_screenshot()
+
+def click_next_btn():
+    take_screenshot()
+    attempts = 0
+    while attempts < 10:
+        try:
+            all_divs = find_elements(By.TAG_NAME, 'div')
+            btn_as_list = list(filter(lambda el: 'View later trains' in el.text and len(el.text) == 17, all_divs))
+            if len(btn_as_list) == 0:
+                pass
+            else:
+                btn_as_list[0].click()
+                attempts = 0
+                break
+        except StaleElementReferenceException:
+            attempts += 1
+            time.sleep(1)
+            print(f'stale exception #{attempts} in click_next_btn() occured')
+        finally:
+            if attempts > 0:
+                print('stale exception for click_next_btn() is now clear')
+                break
 
 def clear_collection(collection_name):
     collection_ref = db.collection(collection_name)
@@ -181,8 +194,11 @@ def clear_collection(collection_name):
             break
         docs = collection_ref.limit(batch_size).stream()
 
-def place_dates_on_hour_price_data():
+def add_date_to_hour_price_data():
     dates = []
+    times0 = []
+    times1 = []
+    prices = []
     with open('real_data.txt', 'r') as file:
         all_lines = file.readlines()
         hour1 = 0
@@ -193,10 +209,29 @@ def place_dates_on_hour_price_data():
                 selected_date += timedelta(days=1)
 
             dates.append(f"{selected_date.strftime(r'%d/%m/%Y')}")
+            times0.append(stripped_line[:5])
+            times1.append(stripped_line[6:11])
+            prices.append(stripped_line[12:])
+
             hour1 = hour2
 
     with open('real_data.txt','w') as file:
-        file.writelines(dates[i] + all_lines[i] for i in range(len(all_lines)))
+        if len(dates) == len(times0):
+            for i in range(len(times0)):
+                file.writeline(f'{dates[i]},{times0[i]},{times1[i]},{prices[i]}')
+        else:
+            print('Lengths of dates and times data are different')
+
+def get_next_trip_q_string():
+    with open('real_data.txt','r') as file:
+        lines = file.readlines()
+        if lines:
+            last_line = lines[-1]
+            last_date = last_line[:10]
+            last_date = datetime.strptime(r'%d/%m/%Y', last_date).strftime(r'%Y-%m-%d')
+            last_hour = last_line[10:12]
+            new_trip_com_q_string = f'https://uk.trip.com/trains/list?departurecitycode=GB2278&arrivalcitycode=GB1594&departurecity=Sheffield&arrivalcity=London%20(Any)&departdate={last_date}&departhouript={last_hour}&departminuteipt=00&scheduleType=single&hidadultnum=1&hidchildnum=0&railcards=%7B%22YNG%22%3A1%7D&isregularlink=1&biztype=UK&locale=en-GB&curr=GBP'
+            return new_trip_com_q_string
 
 def show_all_scrapped_data_for_vid():
     with open('real_data.txt', 'r') as txt_file:
@@ -209,47 +244,55 @@ def convert_seconds(seconds):
 
 if __name__ == '__main__':
     program_tries = 0
-    try:
-        data_length = 0
-        setup_data_file()
+    while program_tries < 5:
         driver = webdriver.Chrome()
         action_chains = ActionChains(driver, 100)
-
         driver.get(trip_com_q_string)
-        decline_cookies()
-        time.sleep(2)
+        try:
+            data_length = 0
+            setup_data_file()
+            
+            decline_cookies()
+            time.sleep(2)
 
-        start_t = time.time()
+            start_t = time.time()
 
-        while data_length < DATA_POINTS:
-            times0 = get_times()[::2]
-            times1 = get_times()[1::2]
-            times1 = list(map(lambda hour: hour[:5], times1))
-            prices = get_prices()
+            while data_length < DATA_POINTS:
+                times0 = get_times()[::2]
+                times1 = get_times()[1::2]
+                times1 = list(map(lambda hour: hour[:5], times1))
+                prices = get_prices()
 
-            if len(times0) < 1 or len(prices) < 1 or len(times1) < 1:
-                continue
+                if len(times0) < 1 or len(prices) < 1 or len(times1) < 1:
+                    continue
 
-            for i in range(len(times0)):
-                write_to_txt_file(times0[i],times1[i],prices[i])
+                for i in range(len(times0)):
+                    write_to_txt_file(times0[i],times1[i],prices[i])
 
-            scroll_to_next_btn(action_chains)
-            click_next_btn()
-            time.sleep(0.3)
-            data_length = count_lines_in_txt_file()
-            print(data_length)
-        else:
-            if DATA_POINTS < 50:
-                show_all_scrapped_data_for_vid()
+                scroll_to_next_btn(action_chains)
+                click_next_btn()
+                time.sleep(0.3)
+                data_length = count_lines_in_txt_file()
+                print(data_length)
+            else:
+                if DATA_POINTS < 50:
+                    show_all_scrapped_data_for_vid()
 
-            time_taken = convert_seconds(time.time() - start_t)
-            print(f'Trip.com was scrapped successfully in {time_taken}')
+                time_taken = convert_seconds(time.time() - start_t)
+                print(f'Trip.com was scrapped successfully in {time_taken}')
+                raise TypeError('On Purpose')
+        except Exception as err:
+            # file = "not-sound-1.wav"
+            # os.system("afplay " + file)
+            print('Something unexpected happened')
+            print('This is what happened ', err)
+            print(f'Trying again: Try #{program_tries}')
+            traceback.print_exc()
+            print('-------------------')
+            program_tries += 1
+            add_date_to_hour_price_data()
+            trip_com_q_string = get_next_trip_q_string()
+            print(trip_com_q_string)
 
-    except Exception as err:
-        file = "not-sound-1.wav"
-        os.system("afplay " + file)
-        print('Something unexpected happened')
-        print('This is what happened ', err)
-        traceback.print_exc()
 
-    driver.quit()
+        driver.quit()
